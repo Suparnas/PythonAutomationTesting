a6270395a03ff16ee64ab16e87ea9a0ab1acbc71
Suparna <dsouza.supy14@gmail.com>
2024-10-29 14:08:23 -0400

    challenge 2 added

--- Exercise Files/CH02/02_07/completed/challenge_02.py
+++ Exercise Files/CH02/02_07/completed/challenge_02.py
@@ -1,18 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 driver.get('https://selenium.dev')
 
-element_id = driver.find_element_by_id('gsc-iw-id1')
-print(element_id)
+# element_id = driver.find_element(By.ID,'gsc-iw-id1')
+# print(element_id)
 
-element_name = driver.find_element_by_name('submit')
-print(element_name)
+# element_name = driver.find_element(By.NAME,'submit')
+# print(element_name)
 
-element_xpath = driver.find_element_by_xpath("//section[@class = 'hero homepage']/h1[1]")
+element_xpath = driver.find_element(By.XPATH,'//*[contains(text(),"Selenium automates browsers")]')
 print(element_xpath)
 
-element_classname = driver.find_element_by_class_name('selenium-backers')
-print(element_classname)
+etext = element_xpath.text
+print(etext)
+
+# element_classname = driver.find_element(By.CLASS_NAME,'selenium-backers')
+# print(element_classname)
 
 driver.close()
