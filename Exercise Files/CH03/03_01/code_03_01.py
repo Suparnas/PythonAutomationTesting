7e38b645a1e21cb69a16bc12369e8879c1fe394c
Suparna <dsouza.supy14@gmail.com>
2024-11-01 16:03:21 -0400

    search added

--- Exercise Files/CH03/03_01/code_03_01.py
+++ Exercise Files/CH03/03_01/code_03_01.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 driver= webdriver.Firefox()
 driver.get("http://python.org");
 
-search = driver.find_element_by_name('q');
+search = driver.find_element(By.NAME,'q');
 search.clear();
 search.send_keys("pycon");
 search.send_keys(Keys.RETURN);
