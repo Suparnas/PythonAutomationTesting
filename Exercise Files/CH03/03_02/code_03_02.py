80aca90d2dbf75575e90b977a7ba4a33886375ab
Suparna <dsouza.supy14@gmail.com>
2024-11-01 16:14:55 -0400

    DROPDOWN ADDED

--- Exercise Files/CH03/03_02/code_03_02.py
+++ Exercise Files/CH03/03_02/code_03_02.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 import time
 from selenium.webdriver.support.ui import Select
 
 driver= webdriver.Firefox()
-driver.get("file:///C:/Users/Bhoomika%20Agarwal/Desktop/Exercise%20Files/CH03/03_02/html_code_03_02.html")
+driver.get("file:///Users/suparnasoman/Documents/SeleniumPython/PythonAutomationTesting/Exercise%20Files/CH03/03_02/html_code_03_02.html")
 
-select= Select(driver.find_element_by_name('numReturnSelect'))
+select= Select(driver.find_element(By.NAME,'numReturnSelect'))
 select.select_by_index(4)
 time.sleep(2)
 select.select_by_visible_text("200")
@@ -16,7 +18,7 @@ time.sleep(2)
 options = select.options
 print(options)
 
-submit_button = driver.find_element_by_name('continue')
+submit_button = driver.find_element(By.NAME,'continue')
 submit_button.submit();
 time.sleep(2)
 
