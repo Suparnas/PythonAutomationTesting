29529e488e224c3f3e73b575bf2e722705518a56
Suparna <dsouza.supy14@gmail.com>
2024-11-03 14:00:35 -0500

    darg drop added and modified

--- Exercise Files/CH03/03_03/dragdrop.py
+++ Exercise Files/CH03/03_03/dragdrop.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 import time
 from selenium.webdriver import ActionChains
 
@@ -8,9 +10,9 @@ driver.switch_to.frame(0)
 
 action_chains= ActionChains(driver)
 
-source= driver.find_element_by_id('draggable')
-target = driver.find_element_by_id('droppable')
-action_chains.drag_and_drop_by_offset(source, 100, 100).perform()
+source= driver.find_element(By.ID,'draggable')
+target = driver.find_element(By.ID,'droppable')
+action_chains.drag_and_drop_by_offset(source, 200, 100).perform()
 time.sleep(2)
 
 action_chains.drag_and_drop(source, target).perform()
