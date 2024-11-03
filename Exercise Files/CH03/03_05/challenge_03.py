90ce449210336da94b5d55ef7b7254fc02f0d680
Suparna <dsouza.supy14@gmail.com>
2024-11-03 14:35:33 -0500

    challenge for select added

--- Exercise Files/CH03/03_05/challenge_03.py
+++ Exercise Files/CH03/03_05/challenge_03.py
@@ -1,18 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 driver = webdriver.Firefox()
 driver.get('https://wiki.python.org/moin/FrontPage')
 
-searchBox= driver.find_element_by_id('searchinput')
+searchBox= driver.find_element(By.ID,'searchinput')
 searchBox.clear()
 searchBox.send_keys("Beginner")
 searchBox.send_keys(Keys.RETURN)
 time.sleep(5)
 
-select = Select(driver.find_element_by_xpath('//*/form/div/select'))
+# select = Select(driver.find_element(By.XPATH,'//*/form/div/select'))
+# select.select_by_visible_text('Print View')
+# time.sleep(5)
+
+
+select = Select(driver.find_element(By.XPATH,'//select[@name="action"]'))
 select.select_by_visible_text("Raw Text")
 time.sleep(5)
 
